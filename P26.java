a8746d305896764be555e9d699db4bcb38724093
DhayanandhS <dhaya.pit180@gmail.com>
2024-07-19 23:00:07 +0530

    Maximum Subarray

---
+++ P26.java
@@ -0,0 +1,14 @@
+class P26 {
+    public int maxSubArray(int[] nums) {
+        int sum = 0;
+        int max = Integer.MIN_VALUE;
+        for(int i = 0; i < nums.length; i++){
+            sum += nums[i];
+            max = Math.max(sum,max);
+            if(sum < 0){
+                sum = 0;
+            }
+        }
+        return max;
+    }
+}
