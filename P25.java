755d0b4462a21c9ff92e367dbbe7dde8954153c0
DhayanandhS <dhaya.pit180@gmail.com>
2024-07-19 17:23:10 +0530

    Two Sum

--- P25.java
+++ P25.java
@@ -1,8 +1,8 @@
 public class P25 {
     public static String read(int n, int []book, int target){
         for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++){
-                if(i == j) continue;
+            for(int j = i + 1; j < n; j++){
+                
                 if(book[i] + book[j] == target){
                     return "YES";
                 }
