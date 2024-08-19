dc6471579a76d029a17a9bc86cb1674bd97edfbd
DhayanandhS <dhaya.pit180@gmail.com>
2024-08-19 19:18:32 +0530

    Character hashing

--- P46.java
+++ P46.java
@@ -9,7 +9,7 @@ class P46 {
         s = sc.next(); // getting string i/p
 
         //precompute:
-        int[] hash = new int[256];
+        int[] hash = new int[26];
         for (int i = 0; i < s.length(); i++) {
             hash[s.charAt(i) - 'a']++;  // this line will directly add the count to the mapped hash index
         }
@@ -18,9 +18,25 @@ class P46 {
         q = sc.nextInt();
         while (q-- > 0) {
             char c;
-            c = sc.next().charAt(0);
+            c = sc.next().charAt(0); // refer notes
             // fetch:
             System.out.println(hash[c - 'a']);
+        }
+
+
+        // this will run efficient with mapping the char in bigger sized array
+        int[] hashh = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            hash[s.charAt(i)]++;  // this line will directly add the count to the mapped hash index
+        }
+
+        int qq;
+        qq = sc.nextInt();
+        while (qq-- > 0) {
+            char c;
+            c = sc.next().charAt(0); // refer notes
+            // fetch:
+            System.out.println(hashh[c]);
         }
     }
 }
