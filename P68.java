ec031b2950c1b0f3907b96a2b836fb07e451875b
DhayanandhS <dhaya.pit180@gmail.com>
2024-08-29 19:27:52 +0530

    404 Not Found

---
+++ P68.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc =new Scanner(System.in);
+		
+		    int a = sc.nextInt();
+		    if(a == 404){
+		        System.out.println("NOT FOUND");
+		    }
+		    else{
+		        System.out.println("FOUND");
+		    }
+		
+	}
+}
