f6c4b06d16d7ba71ac65c27e1700a4be770a7284
DhayanandhS <dhaya.pit180@gmail.com>
2024-09-02 23:38:43 +0530

    Bone Appetit

---
+++ P81.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner scn = new Scanner(System.in);
+		int n  = scn.nextInt();
+		int m = scn.nextInt();
+		int x = scn.nextInt();
+		int y = scn.nextInt();
+		
+		System.out.println((n*x)+(m*y));
+	}
+}
